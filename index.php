7483940d52a18c0a5f34af5955413ba043326873
Preston Kemp <preston@prestonkemp.com>
2021-01-21 22:21:52 -0600

    update sara's message to be template part

--- index.php
+++ index.php
@@ -16,21 +16,9 @@ get_header(); ?>
 
 	<section id="primary" class="content-area col-sm-12 col-md-12 col-lg-12">
 		<div id="main" class="site-main" role="main">
-            <div class="container sara-home">
-                <div class="row">
 
-                    <div class="col-sm">
-                        <img class="photo-home" src="https://static.freemomhugs.org/wp-content/uploads/2020/03/30004403/SaraCunningham_Headshot.jpg"/>
-                    </div>
-                    <div class="col-sm">
-                        <h1>From The Founder:</h1>
-                        <h2>Enough is Enough</h2>
-                        <p>I’m a Christian mom with a gay kid living in Oklahoma saying enough is enough. If I don’t fight for my son (and his rights) like my hair is on fire, then who will? It’s time we celebrate our LGBTQ+ children. And I won’t stop until I no longer hear horror stories from the LGBTQ+ community and their mothers.</p>
-                        <img src="https://static.freemomhugs.org/wp-content/uploads/2020/03/30021112/sara-signature-2.png"/>
-                    </div>
-                </div>
-            </div>
 		<?php
+        get_template_part( 'template-parts/sara-enough-enough');
 		if ( have_posts() ) :
 
 			if ( is_home() && ! is_front_page() ) : ?>
